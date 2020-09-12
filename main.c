5e7c2da04816033932631a5854d40b59aa4a504e
Georges TAVERNE <gtaverne@e3r4p22.42.fr>
2020-09-13 00:15:14 +0200

    +7+8+9

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gtaverne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/11 13:17:34 by gtaverne          #+#    #+#             */
-/*   Updated: 2020/09/12 22:27:20 by gtaverne         ###   ########.fr       */
+/*   Updated: 2020/09/13 00:10:51 by gtaverne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,28 @@
 #include "ex04/ft_str_is_lowercase.c"
 #include "ex05/ft_str_is_uppercase.c"
 #include "ex06/ft_str_is_printable.c"
+#include "ex07/ft_strupcase.c"
+#include "ex08/ft_strlowcase.c"
+#include "ex09/ft_strcapitalize.c"
 
-void new_ex(void)
+void new_ex(int * exo)
 {
-	printf("\n_________________\nNew exo\n");
+	printf("\n_________________\nNew exo :%d \n", *exo );
+	(*exo)++;
 }
 
 int main()
 {
-	new_ex();
+	int ex = 0;
+
+	new_ex(&ex);
 	char dest[150] = {0};
 	char src[] =  "I want to break free";
 	printf("Source: %s\n", src);
 	printf("Return of the function itself: %s\n", ft_strcpy(dest, src));
 	printf("Return of the modified param: %s\n", dest);
 
-	new_ex();
+	new_ex(&ex);
 	char dest2[150] = {0};
 	char src2[] = "He said burn them all";
 	unsigned int usk = 10;
@@ -42,27 +48,45 @@ int main()
 	printf("Return of the function itself: %s\n", ft_strncpy(dest2, src2, usk));
 	printf("Return of the modified param: %s\n", dest2);
 	
-	new_ex();
+	new_ex(&ex);
 	char ex2tst[] = "bond,agent007";
 	char ex2[] = "Youpie";
 	printf("\n test 1 : %s : %d", ex2tst, ft_str_is_alpha(ex2tst));
 	printf("\n test 2 : %s : %d", ex2, ft_str_is_alpha(ex2));
 
-	new_ex();
+	new_ex(&ex);
 	printf("\n test 1 : %s : %d", "50", ft_str_is_numeric("50"));
 	printf("\n test 2 : %s : %d", ex2, ft_str_is_numeric(ex2));
 	
-	new_ex();	
+	new_ex(&ex);	
 	printf("\n test 1 : %s : %d", ex2, ft_str_is_lowercase(ex2));
 	printf("\n test 2 : %s : %d", "50", ft_str_is_lowercase("50"));
 	
 
-	new_ex();	
+	new_ex(&ex);	
 	printf("\n test 1 : %s : %d", ex2, ft_str_is_uppercase(ex2));
-	printf("\n test 1 : %s : %d", "BANZAIIII", ft_str_is_uppercase("BANZAIIII"));
+	printf("\n test 2 : %s : %d", "BANZAIIII", ft_str_is_uppercase("BANZAIIII"));
 	
-	new_ex();	
+	new_ex(&ex);	
 	printf("\n test 1 : %s : %d", "BANZ AIIII", ft_str_is_printable("BANZ  AIIII"));
-	printf("\n test 1 : %s : %d", "tait nucleus \n", ft_str_is_printable("tait nucleus \n"));
-		
+	printf("\n test 2 : %s : %d", "tait nucleus \n", ft_str_is_printable("tait nucleus \n"));
+	
+	new_ex(&ex);		
+	printf("%s\n", ex2);
+	printf("\n test 1 : %s : %s", ex2, ft_strupcase(ex2));
+	char exMAJ[] = "restons calmes!";
+	printf("\n test 2 : %s : %s", exMAJ, ft_strupcase(exMAJ));
+	
+	new_ex(&ex);
+
+	printf("%s\n", ex2);
+	printf("\n test 1 : %s : %s", ex2, ft_strlowcase(ex2));
+	printf("\n test 2 : %s : %s", exMAJ, ft_strlowcase(exMAJ));
+
+	new_ex(&ex);
+	printf("\n test 1 : %s : %s", ex2, ft_strcapitalize(ex2));
+	printf("\n test 2 : %s : %s", exMAJ, ft_strcapitalize(exMAJ));
+
 }
+
+
